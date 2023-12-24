cd40070456916e883680410afc903ad6ddc9055d
MeerMuneeb <jimmykuddon@gmail.com>
2023-12-24 21:36:03 +0500

    ok

---
+++ models/quiz.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema({
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Teacher',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  questions: [
+    {
+      questionText: {
+        type: String,
+        required: true,
+      },
+      options: [
+        {
+          text: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+      correctOption: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Quiz = mongoose.model('Quiz', quizSchema);
+
+module.exports = Quiz;
+
+// FOR TESTING
+// {
+//     "createdBy": "teacherId",  
+//     "title": "Sample Quiz",
+//     "questions": [
+//       {
+//         "questionText": "What is 2 + 2?",
+//         "options": [
+//           {
+//             "text": "3"
+//           },
+//           {
+//             "text": "4"
+//           },
+//           {
+//             "text": "5"
+//           },
+//           {
+//             "text": "6"
+//           }
+//         ],
+//         "correctOption": 1
+//       },
+//     ]
+//   }
+  
--- routes/teacher.js
+++ routes/teacher.js
@@ -1,4 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
+const express = require('express');
+const router = express.Router();
+const teacherController = require('../controllers/teacher');
+
+// Route to add a quiz
+router.post('/addquiz', teacherController.addQuiz);
+
 module.exports = router;
