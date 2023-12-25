455b7e68255c595be4c64098f90308a1b8b0f9b2
Amna Kamran <101377410+amna-kamran@users.noreply.github.com>
2023-12-25 20:58:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,14 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose"); // Add mongoose
-const resultRoutes = require("./routes/result.js");
-const materialRoutes = require("./routes/material.js");
 
 //define the routers
 //e.g
 // var indexRouter = require("./routes/index");
 // var usersRouter = require("./routes/users");
-app.use("/", require("./routes/index"));
 var studentRouter = require("./routes/student");
 
 var teachersRouter = require("./routes/teacher");
-app.use("/results", resultRoutes);
-app.use("/materials", materialRoutes);
 
 var app = express();
 
@@ -46,6 +41,76 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/student", studentRouter);
 
 app.use("/teachers", teachersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+// Server Listening
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`Server is Successfully Running on ${PORT}`)
+);
+
+module.exports = app;
+
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose"); // Add mongoose
+
+//define the routers
+//e.g
+// var indexRouter = require("./routes/index");
+// var usersRouter = require("./routes/users");
+var studentRouter = require("./routes/student");
+
+var teachersRouter = require("./routes/teacher");
+const resultRoutes = require("./routes/result.js");
+const materialRoutes = require("./routes/material.js");
+
+var app = express();
+
+// MongoDB Connection
+const uri =
+  "mongodb+srv://amnakamran139:vgDCvSZfo8XtnrAB@cluster0.qg8esqx.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.warn("An error occurred", err));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+//define the usage of the routers
+//e.g
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
+
+app.use("/student", studentRouter);
+
+app.use("/teachers", teachersRouter);
+
+app.use("/results", resultRoutes);
+app.use("/materials", materialRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
